f254c38ca0e1e545bb07214b3eca9c457fd21705
liuxy <liuxy@kd-tech.com.cn>
2017-07-31 15:55:02 +0800

    upgrade to 0.46

--- android/app/src/main/java/com/awesomeproject/MainApplication.java
+++ android/app/src/main/java/com/awesomeproject/MainApplication.java
@@ -14,6 +14,7 @@ import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.react.uimanager.ViewManager;
+import com.facebook.soloader.SoLoader;
 
 import io.realm.react.RealmReactPackage;
 
@@ -27,12 +28,13 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     public void onCreate() {
         super.onCreate();
+        SoLoader.init(this, /* native exopackage */ false);
         //getReactNativeHost().getReactInstanceManager().createReactContextInBackground();
     }
 
     private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
         @Override
-        protected boolean getUseDeveloperSupport() {
+        public boolean getUseDeveloperSupport() {
             return BuildConfig.DEBUG;
         }
 
